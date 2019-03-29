4337bb11a6171b5d4c4a368bb7a4711874691acb
itan187 <itaag85@gmail.com>
2019-03-29 17:12:59 -0600

    espacio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,10 @@
                                     <div class="modal-header">
                                             <h4 class="modal-title">Agregar</h4>
                                             <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    </div>
+                                  
+                                  
+                                  
+                                        </div>
                                     <div class="modal-body">
                                         <div class="row">
                                                 <div class="col-6">
